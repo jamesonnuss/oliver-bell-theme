ec8d5d9445b43c4df3e0760f49687430962c6299
Jameson Nuss <jamesonnuss@yahoo.com>
2018-07-16 10:39:58 -0700

    Facet WP Filtering Addition

--- template-ico-reviews.php
+++ template-ico-reviews.php
@@ -8,8 +8,11 @@ get_header(); ?>
 	<div class="content">
 
 		<?php get_template_part( 'parts/content', 'header' ); ?>
-		<?php echo facetwp_display( 'facet', 'search' ); ?>
-		<?php echo facetwp_display( 'facet', 'categories' ); ?>
+		<div class="filter-container">
+			<?php echo facetwp_display( 'facet', 'search' ); ?>
+			<?php echo facetwp_display( 'facet', 'categories' ); ?>
+			<button class="button" onclick="FWP.reset()">Clear Filter</button>
+		</div>
 		<?php echo facetwp_display( 'template', 'ico_reviews' ); ?>
 
 	</div> <!-- end #content -->
